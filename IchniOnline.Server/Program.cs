e9c6a29a3e2fc44683cdf8ed353a4c16c56d3d79
Kexun Liu <32810212+stevesensei@users.noreply.github.com>
2026-04-20 17:37:14 +0800

    bug fix

--- IchniOnline.Server/Program.cs
+++ IchniOnline.Server/Program.cs
@@ -17,9 +17,21 @@ builder.Services.AddProblemDetails();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var preferredConnectionNames = new[] { "MainDB", "IchniOnline" };
+var resolvedConnectionName = preferredConnectionNames.FirstOrDefault(name =>
+    !string.IsNullOrWhiteSpace(builder.Configuration.GetConnectionString(name)));
+
+if (resolvedConnectionName is null)
+{
+    throw new InvalidOperationException("No database connection string found. Expected one of: MainDB, IchniOnline.");
+}
+
+var resolvedConnectionString = builder.Configuration.GetConnectionString(resolvedConnectionName)!;
+
 builder.Services.AddControllers();
-builder.Services.AddNpgsqlDataSource("MainDB");
-builder.Services.AddDbContext(builder.Configuration,"MainDB");
+builder.Services.AddNpgsqlDataSource(resolvedConnectionString);
+builder.Services.AddDbContext(builder.Configuration, resolvedConnectionName);
 builder.AddRedisClient("cache");
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.SectionName));
@@ -58,25 +70,6 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-
-string[] summaries =
-    ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-var api = app.MapGroup("/api");
-api.MapGet("weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
 
 app.MapDefaultEndpoints();
 app.UseAuthentication();
